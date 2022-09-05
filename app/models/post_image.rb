a6532bccff012740ac327697ef415096b038ceb1
Tsubakurouuu <yzpolice2896@icloud.com>
2022-09-05 09:07:27 +0000

    meshiterro22章

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -9,11 +9,10 @@ class PostImage < ApplicationRecord
 
   def get_image
     unless image.attached?
-      file_path = Rails.root.join('app/assets/images/no_image_jpg')
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
       image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpg')
-    else
-      image
     end
+    image
   end
 
   def favorited_by?(user)
